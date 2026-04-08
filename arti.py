9278e0b2171c64cf36b5c3311b2b25aed96bb223
Cat Rivas <tykale13@gmail.com>
2026-04-07 20:59:15 -0500

    add description

--- arti.py
+++ arti.py
@@ -2,3 +2,4 @@ print("--------------- ART3MISE ART ------------------")
 
 print('Here will be the ARTI dedication page')
 
+print("THIS GUNTER IS THE FIRST TO FIND THE FIRST CHALLENGE FOR THE COOPER KEY")
